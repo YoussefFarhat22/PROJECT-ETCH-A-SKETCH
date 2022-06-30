c51f07966711ee7b5acff9aa09211596b16e3a74
YoussefFarhat22 <contactyousseffarhat@gmail.com>
2022-06-30 13:51:11 +0100

    Add prompt to users

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ function makeGrid(rows,cols){
     for(let i = 0; i < (rows*cols) ; i++) {
         let squares = document.createElement('div');
         container.appendChild(squares).className = 'grid-items';
+        
         }
     
     };
 makeGrid(16,16);
 
-
 const cells = document.querySelectorAll('.grid-items');
+
+//Set random color (rainbow)
 const randColor = function getRandomColor() {
     let letters = '0123456789ABCDEF';
     let color = '#';
@@ -23,16 +25,71 @@ const randColor = function getRandomColor() {
   }
 
 cells.forEach((cell) => {
-    cell.addEventListener('click', function changeColor(){
+    cell.addEventListener('mouseover', function changeColor(){
         cell.style.backgroundColor = randColor() ;
         
     })
+
 
     const button = document.getElementById('clear');
     button.addEventListener('click',function clean(){
         cell.style.backgroundColor = 'white';
     });
 });
+
+
+
+    
+    
+
+
+
+//create new grid
+const btn = document.getElementById('numberOfgrid');
+btn.addEventListener('click',()=>{
+     const n = prompt('Enter number of squares','64');
+     if(n>64){
+        prompt('Enter a number under 64 please!!',)
+     }
+     createNewGrid(n);
+     
+
+
+     
+});
+
+function createNewGrid(n){
+    let grids = container.querySelectorAll('div');
+    grids.forEach(grid=>
+        grid.remove()
+        );
+       makeGrid(n,n);
+    let squares2 = document.querySelectorAll('.grid-items')
+    squares2.forEach((square2)=>{
+        square2.addEventListener('mouseover',()=>
+        square2.style.backgroundColor = randColor());
+
+        const button = document.getElementById('clear');
+        button.addEventListener('click',function clean(){
+            square2.style.backgroundColor = 'white';
+        });
+
+
+
+
+
+    });
+    
+
+
+       
+
+    }
+
+
+
+    
+
 
 
 
